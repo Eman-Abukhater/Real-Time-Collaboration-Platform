4655fc97ba0d2d4afe1feba2f296d349c23e32d1
Eman <emanabukhater2001@gmail.com>
2025-07-15 21:08:23 +0300

    Change the role value inside register to Admin temporarily

--- backend/src/graphql/resolvers.ts
+++ backend/src/graphql/resolvers.ts
@@ -29,7 +29,7 @@ export const resolvers = {
         username,
         email,
         password: hashedPassword,
-        role: "User"
+        role:  "Admin"
       };
 
       users.push(newUser);

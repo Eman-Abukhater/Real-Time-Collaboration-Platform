5cc9bf7976b8d599d3020ce119546f08344ab5a0
Eman <emanabukhater2001@gmail.com>
2025-07-15 16:21:22 +0300

    Create base structure

--- backend/src/resolvers/hello.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from 'type-graphql';
-
-@Resolver()
-export class HelloResolver {
-  @Query(returns => String) // Alternative syntax that always works
-  hello(): string {
-    return "Test successful! Backend is working!";
-  }
-}

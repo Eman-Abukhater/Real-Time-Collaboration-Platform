7f462b8620958f0463769ae109de8e91803ccfa8
Eman <emanabukhater2001@gmail.com>
2025-07-15 19:14:53 +0300

    Create a User Model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: string;
+    username: string;
+    email: string;
+    password: string; // hashed
+    role: "Admin" | "User" | "Guest";
+  }
+  
+  export const users: User[] = [];
+  
